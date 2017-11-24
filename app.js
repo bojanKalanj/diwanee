5201d37b2400cc3e7a7f358086e8b1784aba6315
bojanKalanj <bojan.kalanj@gmail.com>
2017-11-24 18:18:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var icon = document.querySelector('#icon');
+var dropdown = document.querySelector('#dropdown');
+var dropdownSocial = document.querySelector('#dropdown-social');
+var isOn = false;
+
+icon.addEventListener('click', function(){
+    icon.classList.toggle('active');
+    if(isOn){
+        dropdown.style.display = 'none';
+        dropdownSocial.style.display = 'none';        
+    }else{
+        dropdown.style.display = 'block';
+        dropdownSocial.style.display = 'block';        
+    }
+    isOn = !isOn;    
+})
